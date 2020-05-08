7fc488a72ddd566b141d717cfd8c9a41bf466c6e
Matt Farncombe <matt@farncombe.co>
2020-05-08 02:16:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,12 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(calls)
-  
+  calls.each do |call|
+    if call.length > 4
+      return true
+    end
+    return false
+  end  
 end
 
 def find_the_cheese(foods)
